02e9962eea410d9030fdfe3c57c5ade4d6ff16ae
unknown <pedrohenriquelouresoliveira@gmail.com>
2022-10-02 00:34:16 -0300

    interface para endpoints

---
+++ repositories/IJogoRepository.cs
@@ -0,0 +1,20 @@
+﻿using Jogos_API.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Jogos_API.repositories
+{
+    public interface IJogoRepository
+    {
+        Task<IEnumerable<Jogo>> Get();
+
+        Task<Jogo> Get(int id);
+
+        Task<Jogo> Create(Jogo jogo);
+
+        Task<Jogo> Update(Jogo jogo);
+
+        Task<Jogo> Delete(int id);
+    }
+}
